5d8d74810fba2b6f53246a429727f08cf75e513b
Dima Claudiu <yatsukoyuno@gmail.com>
2020-01-16 23:33:32 +0200

    Update 2048.c

--- 2048.c
+++ 2048.c
@@ -6,5 +6,5 @@ int main(void)
 	initializeWindow(); //doing all the initialization at the start
 	menu(0); //staring with the menu and a non resumeable game, the rest goes from there
 
-	return 0; //won't ever come to this but oh well
+	return 0;
 }
